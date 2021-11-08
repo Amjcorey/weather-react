d4d8e4dd9d3e4abea71f8c39cad888aed193e180
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-07 18:21:52 -0800

    added rel and value to link in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
             Weather app
             <footer>
                 Coded by Amanda Corey and is {""}
-                <a href="https://github.com/Amjcorey/weather-react" target="_blank">open-sourced</a> on GitHub.
+                <a rel="noreferrer" href="https://github.com/Amjcorey/weather-react" target="_blank">open-sourced</a> on GitHub.
             </footer>
         </div>
     );
