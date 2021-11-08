1719558e198bf266a6d26fc9c6a0e1a7c8063f4f
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-07 19:00:41 -0800

    added bootstrap, updated app.js and attempted to change setting to auto indent code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-    return (
-        <div className="App">
-            Weather app
-            <footer>
-                Coded by Amanda Corey and is {""}
-                <a rel="noreferrer" href="https://github.com/Amjcorey/weather-react" target="_blank">open-sourced</a> on GitHub.
-            </footer>
-        </div>
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App </h1>
+         <footer>
+                Coded by Amanda Corey and is{" "}
+            <a
+              rel="noreferrer"
+              href="https://github.com/Amjcorey/weather-react" target="_blank">open-sourced
+            </a>
+            on GitHub.
+          </footer>
+        </div> 
+      </div>
     );
 }
