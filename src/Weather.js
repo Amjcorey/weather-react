f66124b23666a2611d1875fcafce704afd475f47
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-14 15:41:25 -0800

    began creating functions in <form> to call weather, display city; removed font awesome dependencies and extra react-bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,44 @@
-import React from "react";
-// import axios from "axios";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 
 
-export default function Weather() {
 
-  
+export default function Weather(props) {
+const [city, setCity] = useState(props.defaultCity);
+const [weatherData, setWeatherData] = useState({ ready: false });
 
+
+
+function handleResponse(response) {
+  setWeatherData({
+    ready: true,
+    coordinates: response.data.coord,
+
+  })
+}
+
+/* function to search a city */
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+function handleCityDisplay(event) {
+  //triggered by input button element or pressing Enter to search a city
+  setCity(event.target.value)
+}
+  /* pressing Enter or clicking Search button triggers function to get the weather data for the searched city using the OpenWeather API */
+function search() {
+  const apiKey = "70de72ce25d0801c193edd1d17ced422";
+
+  let units = "metric";
+  let apiUrl = 
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+}
 
   
   return (
@@ -28,6 +59,7 @@ export default function Weather() {
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
               alt="Light rain showers"
+              size={52}
             />
             </div>
             <h2>Light rain showers</h2>
@@ -37,24 +69,26 @@ export default function Weather() {
             </div>
           </div>
         </div>
-        <form>
-          <div className="d-flex flex-column"> 
-          <div className="p-2">
-          <input
-            type="search"
-            value="i.e. Seattle"
-            className="form-control input-search"
-          />
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="i.e. Seattle"
+              className="form-control"
+              onChange={handleCityDisplay}
+              autoFocus="on"
+            />
           </div>
-          <div className="p-2">
-          <input
-            type="submit"
-            value="Search city"
-            className="btn btn-primary"
-          />
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
-          </div>
-        </form>
+        </div>
+      </form>
       
     </div>
   );
